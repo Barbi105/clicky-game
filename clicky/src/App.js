443a73cf4876ae122ab16b6a5b44171e4b77178d
barbi105 <bontiverosg@gmail.com>
2019-04-30 10:19:17 -0500

    help me

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
-import FriendCard from "./components/FriendCard";
-import Footer from "./components/Footer";
+import Navbar from "src/components/Navbar";
+import Jumbotron from "src/components/Jumbotron";
+import FriendCard from "src/components/FriendCard";
+import Footer from "src/components/Footer";
 import croc from "./croc.json";
 import "./App.css";
 
