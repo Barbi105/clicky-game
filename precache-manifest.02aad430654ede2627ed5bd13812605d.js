aebd3b89f73f61a9433f1256be5243d33cd84e80
barbi105 <bontiverosg@gmail.com>
2019-04-30 22:02:50 -0500

    Updates

---
+++ precache-manifest.02aad430654ede2627ed5bd13812605d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6327a4eece20b2b9e8c5b37814d34c98",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "70164e9ff7b4809eab71",
+    "url": "/clicky-game/static/css/main.02dea271.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "70164e9ff7b4809eab71",
+    "url": "/clicky-game/static/js/main.db7dc24d.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "0afa2a87ab8fe0df7692a048e8f3fe0a",
+    "url": "/clicky-game/static/media/croc1.0afa2a87.jpg"
+  },
+  {
+    "revision": "6fbfac5ee3a3e53755e560a4d7146071",
+    "url": "/clicky-game/static/media/croc10.6fbfac5e.jpg"
+  },
+  {
+    "revision": "001ead769003b10fdc9891b48d287f96",
+    "url": "/clicky-game/static/media/croc11.001ead76.jpg"
+  },
+  {
+    "revision": "a251bb3cde71c754b661cb9a7f62a359",
+    "url": "/clicky-game/static/media/croc12.a251bb3c.jpg"
+  },
+  {
+    "revision": "7b53d19bffc0764c994d0c66514b872a",
+    "url": "/clicky-game/static/media/croc2.7b53d19b.jpg"
+  },
+  {
+    "revision": "faa1a358c82ee6235ebd4c78ecb47f8a",
+    "url": "/clicky-game/static/media/croc3.faa1a358.jpg"
+  },
+  {
+    "revision": "4bc589c7097a8308e9e219f30824be0c",
+    "url": "/clicky-game/static/media/croc4.4bc589c7.jpg"
+  },
+  {
+    "revision": "f7aa6420288135ef3350ec2b462cb3f5",
+    "url": "/clicky-game/static/media/croc5.f7aa6420.jpg"
+  },
+  {
+    "revision": "905e3812dd983e3ea204ef071b647574",
+    "url": "/clicky-game/static/media/croc6.905e3812.jpg"
+  },
+  {
+    "revision": "be89676a075d346bedeb83a36fb0eab0",
+    "url": "/clicky-game/static/media/croc7.be89676a.jpg"
+  },
+  {
+    "revision": "5d997216261a2b26146ed3358fc2d30f",
+    "url": "/clicky-game/static/media/croc8.5d997216.jpg"
+  },
+  {
+    "revision": "6bcb640820319259908c2c309644baa3",
+    "url": "/clicky-game/static/media/croc9.6bcb6408.jpg"
+  }
+]);
